2b5b87a301a5627322ceb3867b96ae28bb9df94a
chinnapareddy <chinna.aduri@gmail.com>
2023-10-18 19:48:15 -0700

    terraform structure

--- subnet/main.tf
+++ subnet/main.tf
@@ -1,8 +1,11 @@
 resource "aws_subnet" "main" {
-  for_each = var.subnet
-  vpc_id     = var.vpc_id
-  cidr_block = each.value["cidr"]
+  for_each          = var.subnet
+  vpc_id            = var.vpc_id
+  cidr_block        = each.value["cidr"]
   availability_zone = each.value["az"]
+  tags = {
+    Name = each.key
+  }
 }
 
 variable "subnet" {}
