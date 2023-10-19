2da8647bad8aea60b119d6efb13352941e234485
chinnapareddy <chinna.aduri@gmail.com>
2023-10-18 21:32:11 -0700

    terraform structure

--- subnet/main.tf
+++ subnet/main.tf
@@ -17,3 +17,9 @@ for_each = var.subnets
   }
 }
 
+resource "aws_route_table_association" "main" {
+  for_each = var.subnets
+  subnet_id      = lookup(lookup(aws_subnet.main, each.key, null),"id",null)
+  route_table_id = lookup(lookup(aws_route_table.main,each.key, null), "id", null)
+}
+
