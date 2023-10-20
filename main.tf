72275831d52dbb77ec02fb18f6043f55574f949c
chinnapareddy <chinna.aduri@gmail.com>
2023-10-19 23:07:38 -0700

    terraform structure

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ resource "aws_eip" "ngw" {
   domain  = "vpc"
 }
 
-resource "aws_nat_gateway" "ngw" {
+/*resource "aws_nat_gateway" "ngw" {
   # for_each = lookup(lookup(module.subnets, "public", null), "subnet_ids", null)
   # allocation_id = lookup(lookup(aws_eip.ngw, each.key, null), "id", null)
   #subnet_id = each.value["id"]
@@ -42,28 +42,24 @@ resource "aws_nat_gateway" "ngw" {
   subnet_id     = element(local.public_subnet_ids,count.index)
 }
 
-/*resource "aws_route" "ngw" {
+*//*resource "aws_route" "ngw" {
  # for_each               = lookup(lookup(module.subnets, "public", null), "route_table_ids", null)
  # route_table_id         = each.value["id"]
  # destination_cidr_block = "0.0.0.0/0"
  # gateway_id             = aws_internet_gateway.igw.id
-
-  Count                  =  length(local.private_route_table_ids)
-  route_table_id         =  element(local.private_route_table_ids,count.index)
-  destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = element(aws_nat_gateway.ngw.*.id,count.index)
-}*/
+}*//*
 
 resource "aws_route" "ngw" {
   Count                  =  length(local.private_route_table_ids)
   route_table_id         =  element(local.private_route_table_ids,count.index)
   destination_cidr_block = "0.0.0.0/0"
   nat_gateway_id         = element(aws_nat_gateway.ngw.*.id,count.index)
-}
-
-
-
+}*/
 
 output "subnet" {
   value = module.subnets
+}
+
+output "public_subnet_ids" {
+  value = local.public_subnet_ids
 }
